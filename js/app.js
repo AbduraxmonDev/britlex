c61844969c882c7c785c3d88ab7c65751c363b23
abdurahmon <abdurahmon@mail/ru>
2022-12-26 15:20:42 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("burger").addEventListener("click", function() {
+        document.querySelector("header").classList.toggle("open")
+    })
+})
+
+document.addEventListener('keydown', function(e){
+    if (e.key == 'Escape') {
+        document.querySelector("header").classList.remove("open")
+    }
+}) ;
